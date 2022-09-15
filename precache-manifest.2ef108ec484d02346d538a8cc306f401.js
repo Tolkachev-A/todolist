f77cb09e9a425615b3800c58cfdc16fafe0b48c7
Antont25 <toxafootbol1994@gmail.com>
2022-09-15 18:16:29 +0300

    Updates

---
+++ precache-manifest.2ef108ec484d02346d538a8cc306f401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e236067639f1bbdf59506a74e5a61b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "94a57aef304e30607ce4",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "c54787b64f522eebf76b",
+    "url": "/todolist/static/js/2.985f8dbb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.985f8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a57aef304e30607ce4",
+    "url": "/todolist/static/js/main.260f1654.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
